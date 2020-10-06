09b753729297b1fbcdba8fc01f7c4b5ec911d7c6
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-06 09:43:04 -0400

    draw cball and cannon with updateAndDraw()

--- sketch.js
+++ sketch.js
@@ -1,17 +1,28 @@
+//define objects and properties
 let cannon = {}
 cannon.x = 0
 cannon.y = 200
-cannon.width = 100
-cannon.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZggUW1fatrwIxecH0HAw6R7k08yQWM9Q7JeW8qKS5SLtgRBM%3Aclipart-library.com%2Fimage_gallery%2F585873.png&usqp=CAU"
-
-let cball = {}
+cannon.width = 75
+cannon.height = 25
+cannon.vy = 2
+cannon.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/cannon-18-569875.png"
 
 let target = {}
+
+let cball = {}
+cball.x = 0
+cball.y = 200
+cball.width = 20
+cball.height = 20
+cball.vx = 2
+cball.vy = 2
+cball.src = "https://cdn4.iconfinder.com/data/icons/pirate-3/512/as_1119-512.png"
 
 let world = {}
 
 function preload() {
   cannon.img = loadImage(cannon.src)
+  cball.img = loadImage(cball.src)
 }
 
 function setup() {
@@ -19,12 +30,21 @@ function setup() {
 }
 
 function draw() {
-  background('white');
-  image(cannon.img, cannon.x, cannon.y, cannon.width, cannon.width / 2)
+  background("white");
   
-  update(cannon)
+  updateAndDraw(cannon)
+  updateAndDraw(cball)
 }
 
-function update(sprite) {
-  sprite.y += 2
+function updateAndDraw(sprite) {
+  image(sprite.img, sprite.x, sprite.y, sprite.width, sprite.height)
+  //rect(sprite.x, sprite.y, sprite.width, sprite.height)
+  //increase y by vy
+  sprite.y += sprite.vy
 }
+
+
+
+
+
+
