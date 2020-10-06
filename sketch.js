cf62afdb8aca6c82e28e1467106d7f7c1b52a79e
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-06 10:57:28 -0400

    updateAndDraw target

--- sketch.js
+++ sketch.js
@@ -9,11 +9,17 @@ cannon.vy = 2
 cannon.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/cannon-18-569875.png"
 
 let target = {}
+target.x = 350
+target.y = 200
+target.width = 40
+target.height = 40
+target.vy = 2
+target.src = "https://lh3.googleusercontent.com/proxy/15Uuw1SvhtOANNTvx1O0gLDowL8ZUftQBU2AR7zs14pap2GPSDn68Cj8izOQbFO_X6QfuihvcygXjDyPAE4RiJdxyrcSwLPrWvQvP57MFiN4FpPP4ZcFJABhOyEijVkczPF5c5FiqRyxenIaM9cLaOrzZW68000P"
 
 
 function preload() {
   cannon.img = loadImage(cannon.src)
-  
+  target.img = loadImage(target.src)
 }
 
 function setup() {
@@ -23,11 +29,12 @@ function setup() {
 function draw() {
   background("white");
   
-  updateAndDraw()
+  updateAndDraw(cannon)
+  updateAndDraw(target)
 }
 
-function updateAndDraw() {
-  image(cannon.img, cannon.x, cannon.y, cannon.width, cannon.height)
+function updateAndDraw(sprite) {
+  image(sprite.img, sprite.x, sprite.y, sprite.width, sprite.height)
   //increase y by vy
-  cannon.y += cannon.vy
+  sprite.y += sprite.vy
 }
