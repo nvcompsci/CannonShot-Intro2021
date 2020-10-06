6393b5985a9d4dd34ae14d3d9044db9ecb09341f
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-06 11:18:43 -0400

    target moves, world bounds

--- sketch.js
+++ sketch.js
@@ -10,12 +10,15 @@ cannon.src = "https://cdn.iconscout.com/icon/premium/png-512-thumb/cannon-18-569
 
 let target = {}
 target.x = 350
-target.y = 200
+target.y = 50
 target.width = 40
 target.height = 40
-target.vy = 2
+target.vy = -2
 target.src = "https://lh3.googleusercontent.com/proxy/15Uuw1SvhtOANNTvx1O0gLDowL8ZUftQBU2AR7zs14pap2GPSDn68Cj8izOQbFO_X6QfuihvcygXjDyPAE4RiJdxyrcSwLPrWvQvP57MFiN4FpPP4ZcFJABhOyEijVkczPF5c5FiqRyxenIaM9cLaOrzZW68000P"
 
+let world = {}
+world.top = 0
+world.bottom = 400
 
 function preload() {
   cannon.img = loadImage(cannon.src)
@@ -37,4 +40,13 @@ function updateAndDraw(sprite) {
   image(sprite.img, sprite.x, sprite.y, sprite.width, sprite.height)
   //increase y by vy
   sprite.y += sprite.vy
+  
+  collideWorldBounds(sprite)
 }
+
+function collideWorldBounds(sprite) {
+  if (sprite.y > world.bottom ||
+     sprite.y < world.top) {
+    sprite.vy = -sprite.vy
+  }
+} 
