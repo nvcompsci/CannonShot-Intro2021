9126d8e89fc8f1ce81951c8b6927c1738267f340
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-07 09:55:40 -0400

    cannon shows and moves

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 //define objects and properties
 let cannon = {}
+//x, y, width, height, vy
+cannon.x = 50
+cannon.y = 200
+cannon.width = 100
+cannon.height = 75
+cannon.vy = 2
+cannon.src = "https://lh3.googleusercontent.com/proxy/JsjL8jwcGvSa0SRJnQy2NfbuYzW3sxL1DhvYJkgijDRG2O83Gf1qV3kzvcyXeavV5m1-WR0YQXULIP3OE4fPs3A7v7K2bxw"
+
+let cannonBall = {}
+
+let target = {}
+
+let world = {}
 
 function preload() {
-  
+  cannon.img = loadImage(cannon.src)
 }
 
 function setup() {
@@ -10,7 +23,18 @@ function setup() {
 }
 
 function draw() {
-  background("white");
+  background(220);
+  updateAndDraw()
 }
 
-//groupB period3
+function updateAndDraw() {
+  //rect(cannon.x, cannon.y, cannon.width, cannon.height)
+  image(cannon.img, cannon.x, cannon.y, cannon.width, cannon.height)
+  
+  //move down, increase y with vy
+  cannon.y += cannon.vy
+}
+
+
+
+
