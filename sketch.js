7031047b920f5f7bc566b72616442d8a2a5e0d05
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-08 11:09:32 -0400

    draw cannonBall, use updateAndDraw(sprite), start target

--- sketch.js
+++ sketch.js
@@ -5,16 +5,24 @@ cannon.y = 200
 cannon.width = 100
 cannon.height = 75
 cannon.vy = 2
-cannon.src = "https://lh3.googleusercontent.com/proxy/NHk0ci0zbqVYB_R5EIjvaeRiTAgCfeFkgmXuE9AjXG87U6jE-18QAgXPBTmp_bRQ7E55bvVxLnUb8FzeDqRCoozgPHBnvo8"
+cannon.src = "https://lh3.googleusercontent.com/proxy/HdmhTrrAWPEqA1TdOBijRVjHbVunqQ70xtUJE0UscOBxlRNfUPiqWdDpVNtkSU4kw8OhULr1optoRabVCebCyxC6cd0w-v0"
 
 let cannonBall = {}
+cannonBall.x = 50
+cannonBall.y = 100
+cannonBall.width = 30
+cannonBall.height = 30
+cannonBall.vy = 3
+cannonBall.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/153aa662-fcaf-413e-a31c-893bdd6f9629/dbrh1u0-c5bd1786-58c8-4f22-b0bb-93704a687da1.png/v1/fill/w_400,h_468,strp/cannonball_by_crazyfilmmaker_dbrh1u0-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD00NjgiLCJwYXRoIjoiXC9mXC8xNTNhYTY2Mi1mY2FmLTQxM2UtYTMxYy04OTNiZGQ2Zjk2MjlcL2RicmgxdTAtYzViZDE3ODYtNThjOC00ZjIyLWIwYmItOTM3MDRhNjg3ZGExLnBuZyIsIndpZHRoIjoiPD00MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.1zeb-ST3vqrF0QbtvjImEAXKOGv4TysLk6m0h0V8ZGg"
 
 let target = {}
+target.x = 350
 
 let world = {}
 
 function preload() {
   cannon.img = loadImage(cannon.src)
+  cannonBall.img = loadImage(cannonBall.src)
 }
 
 function setup() {
@@ -24,18 +32,23 @@ function setup() {
 function draw() {
   background(220);
   
-  updateAndDraw()
+  updateAndDraw(cannon)
+  updateAndDraw(cannonBall)
 }
 
-function updateAndDraw() {
+function updateAndDraw(sprite) {
   //rect(cannon.x, cannon.y, cannon.width, cannon.height)
-  image(cannon.img , cannon.x, cannon.y, cannon.width, cannon.height)
+  image(sprite.img , sprite.x, sprite.y, sprite.width, sprite.height)
   
   //move cannon: y increases by vy
-  cannon.y += cannon.vy
+  sprite.y += sprite.vy
+  
+  //bounce off top and bottom
+  if (sprite.y > height - sprite.height ||
+     sprite.y < 0) {
+    sprite.vy = -sprite.vy
+  }
 }
-
-
 
 
 
