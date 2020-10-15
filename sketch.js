ec86d5b54df0c1cf2431cc11ab7e336635c5c669
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-15 10:53:51 -0400

    better image links

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ cannon.y = 200
 cannon.width = 100
 cannon.height = 75
 cannon.vy = 2
-cannon.src = "https://lh3.googleusercontent.com/proxy/HdmhTrrAWPEqA1TdOBijRVjHbVunqQ70xtUJE0UscOBxlRNfUPiqWdDpVNtkSU4kw8OhULr1optoRabVCebCyxC6cd0w-v0"
+cannon.src = "https://raw.githubusercontent.com/nvcompsci/CannonShot-Intro2021/main/cannon.png"
 
 let cannonBall = {}
 cannonBall.x = 50
@@ -13,7 +13,7 @@ cannonBall.y = 100
 cannonBall.width = 30
 cannonBall.height = 30
 cannonBall.vy = 3
-cannonBall.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/153aa662-fcaf-413e-a31c-893bdd6f9629/dbrh1u0-c5bd1786-58c8-4f22-b0bb-93704a687da1.png/v1/fill/w_400,h_468,strp/cannonball_by_crazyfilmmaker_dbrh1u0-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD00NjgiLCJwYXRoIjoiXC9mXC8xNTNhYTY2Mi1mY2FmLTQxM2UtYTMxYy04OTNiZGQ2Zjk2MjlcL2RicmgxdTAtYzViZDE3ODYtNThjOC00ZjIyLWIwYmItOTM3MDRhNjg3ZGExLnBuZyIsIndpZHRoIjoiPD00MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.1zeb-ST3vqrF0QbtvjImEAXKOGv4TysLk6m0h0V8ZGg"
+cannonBall.src = "https://raw.githubusercontent.com/nvcompsci/CannonShot-Intro2021/main/cannonball.png"
 
 let target = {}
 target.x = 350
@@ -21,7 +21,7 @@ target.y = 300
 target.width = 50
 target.height = 50
 target.vy = 2
-target.src = "https://smeonline.net/wp-content/uploads/target-clipart-target.png"
+target.src = "https://raw.githubusercontent.com/nvcompsci/CannonShot-Intro2021/main/target.png"
 
 let world = {}
 
@@ -56,6 +56,3 @@ function updateAndDraw(sprite) {
     sprite.vy = -sprite.vy
   }
 }
-
-
-
