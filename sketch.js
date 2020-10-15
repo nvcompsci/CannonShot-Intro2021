8a366ca0350810068b81243782ab009e36f66f1a
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-15 09:29:10 -0400

    target shows, cannon fires

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ cannon.y = 200
 cannon.width = 100
 cannon.height = 75
 cannon.vy = 2
+cannon.vx = 0
 cannon.src = "https://lh3.googleusercontent.com/proxy/HdmhTrrAWPEqA1TdOBijRVjHbVunqQ70xtUJE0UscOBxlRNfUPiqWdDpVNtkSU4kw8OhULr1optoRabVCebCyxC6cd0w-v0"
 
 let cannonBall = {}
@@ -13,7 +14,8 @@ cannonBall.x = 100
 cannonBall.y = 20
 cannonBall.width = 30
 cannonBall.height = 30
-cannonBall.vy = 3
+cannonBall.vy = 0
+cannonBall.vx = 0
 cannonBall.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/153aa662-fcaf-413e-a31c-893bdd6f9629/dbrh1u0-c5bd1786-58c8-4f22-b0bb-93704a687da1.png/v1/fill/w_400,h_468,strp/cannonball_by_crazyfilmmaker_dbrh1u0-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD00NjgiLCJwYXRoIjoiXC9mXC8xNTNhYTY2Mi1mY2FmLTQxM2UtYTMxYy04OTNiZGQ2Zjk2MjlcL2RicmgxdTAtYzViZDE3ODYtNThjOC00ZjIyLWIwYmItOTM3MDRhNjg3ZGExLnBuZyIsIndpZHRoIjoiPD00MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.1zeb-ST3vqrF0QbtvjImEAXKOGv4TysLk6m0h0V8ZGg"
 
 let target = {}
@@ -22,12 +24,14 @@ target.y = 100
 target.width = 50
 target.height = 50
 target.vy = -2
+target.vx = 0
 
 let world = {}
 
 function preload() {
-  cannon.img = loadImage(cannon.src)
-  cannonBall.img = loadImage(cannonBall.src)
+  //cannon.img = loadImage(cannon.src)
+  //cannonBall.img = loadImage(cannonBall.src)
+  //target.img = loadImage(target.src)
 }
 
 function setup() {
@@ -38,14 +42,16 @@ function draw() {
   background(220);
   updateAndDraw(cannon)
   updateAndDraw(cannonBall)
+  updateAndDraw(target)
 }
 
 function updateAndDraw(sprite) {
-  //rect(sprite.x, sprite.y, sprite.width, sprite.height)
-  image(sprite.img, sprite.x, sprite.y, sprite.width, sprite.height)
+  rect(sprite.x, sprite.y, sprite.width, sprite.height)
+  //image(sprite.img, sprite.x, sprite.y, sprite.width, sprite.height)
   
   //move down, increase y with vy
   sprite.y += sprite.vy
+  sprite.x += sprite.vx
   
   
   if (sprite.y > 400 - sprite.height ||
@@ -53,3 +59,18 @@ function updateAndDraw(sprite) {
     sprite.vy = -sprite.vy
   }
 }
+
+function fireCannon() {
+  cannonBall.x = cannon.x
+  cannonBall.y = cannon.y
+  cannonBall.vy = cannon.vy
+  cannonBall.vx = 3
+}
+
+function mouseReleased() {
+  fireCannon()
+}
+
+
+
+
